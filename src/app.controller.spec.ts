23f26a6420750fbd9e99356d58ac5fd0e428d4e0
Fabio Sanchez <holom.dev@gmail.com>
2023-12-05 12:43:39 -0600

    test: refactor health check test

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -14,9 +14,9 @@ describe('AppController', () => {
     appController = app.get<AppController>(AppController);
   });
 
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+  describe('Health Check', () => {
+    it('should return "UP!"', () => {
+      expect(appController.health_check()).toBe('UP!');
     });
   });
 });
