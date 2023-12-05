213f9586d92fc66e90fb42883b5a016b6444a8d7
Fabio Sanchez <holom.dev@gmail.com>
2023-12-05 12:20:23 -0600

    feat: health check end point

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  health_check(): string {
+    return this.appService.health_check();
   }
 }
