c93343ba065e3f68d98bd1606648b9e3938777c8
Fabio Sanchez <holom.dev@gmail.com>
2023-12-05 15:24:40 -0600

    feat: add User entity

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,21 @@
+import { Exclude } from 'class-transformer';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column({ unique: true })
+  username: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  @Exclude()
+  password: string;
+}
