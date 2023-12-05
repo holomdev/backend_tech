48e593be74e9562382442cdfbb0e9615eabdd4c7
Fabio Sanchez <holom.dev@gmail.com>
2023-12-05 15:32:00 -0600

    feat: add BcryptService for hashing passwords

--- src/iam/iam.module.ts
+++ src/iam/iam.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AuthenticationController } from './authentication/authentication.controller';
 import { AuthenticationService } from './authentication/authentication.service';
+import { HashingService } from './hashing/hashing.service';
+import { BcryptService } from './hashing/bcrypt.service';
 
 @Module({
   controllers: [AuthenticationController],
-  providers: [AuthenticationService],
+  providers: [
+    {
+      provide: HashingService,
+      useClass: BcryptService,
+    },
+    AuthenticationService,
+  ],
 })
 export class IamModule {}
