2afe02ba34d596ac038b7f9f9937c08d3bf4934f
Fabio Sanchez <holom.dev@gmail.com>
2023-12-05 15:01:57 -0600

    feat: add authentication controller tests

--- src/iam/authentication/authentication.controller.spec.ts
+++ src/iam/authentication/authentication.controller.spec.ts
@@ -1,18 +1,61 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthenticationController } from './authentication.controller';
+import { AuthenticationService } from './authentication.service';
+import { SignInDto } from './dto/sign-in.dto';
+import { SignUpDto } from './dto/sign-up.dto';
 
 describe('AuthenticationController', () => {
   let controller: AuthenticationController;
+  let authenticationService: AuthenticationService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthenticationController],
+      providers: [
+        {
+          provide: AuthenticationService,
+          useValue: {
+            signUp: jest.fn(),
+            signIn: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<AuthenticationController>(AuthenticationController);
+    authenticationService = module.get<AuthenticationService>(
+      AuthenticationService,
+    );
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should call method signIn in authService', async () => {
+    const signInDto: SignInDto = {
+      email: 'test@example.com',
+      password: 'password123',
+    };
+
+    jest.spyOn(authenticationService, 'signIn');
+    await controller.signIn(signInDto);
+
+    expect(authenticationService.signIn).toHaveBeenCalledWith(signInDto);
+  });
+
+  it('should call method signUp in authService', async () => {
+    const signUpDto: SignUpDto = {
+      email: 'test@example.com',
+      password: 'password123',
+      username: 'username',
+      name: 'name',
+    };
+
+    jest.spyOn(authenticationService, 'signUp');
+
+    await controller.signUp(signUpDto);
+
+    expect(authenticationService.signUp).toHaveBeenCalledWith(signUpDto);
   });
 });
