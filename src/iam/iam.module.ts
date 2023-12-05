30a7f9fc504170daac5aa8fc1ad9ca4794416d73
Fabio Sanchez <holom.dev@gmail.com>
2023-12-05 16:12:05 -0600

    feat: add JWT configuration and dependencies to IAM module

--- src/iam/iam.module.ts
+++ src/iam/iam.module.ts
@@ -5,9 +5,16 @@ import { HashingService } from './hashing/hashing.service';
 import { BcryptService } from './hashing/bcrypt.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '../users/entities/user.entity';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule } from '@nestjs/config';
+import jwtConfig from './config/jwt.config';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    JwtModule.registerAsync(jwtConfig.asProvider()),
+    ConfigModule.forFeature(jwtConfig),
+  ],
   controllers: [AuthenticationController],
   providers: [
     {
