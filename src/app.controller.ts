826e8de71e9a916c6abd03f02f1d135463616552
Fabio Sanchez <holom.dev@gmail.com>
2023-12-05 17:19:13 -0600

    feat: add health check endpoint with Swagger documentation

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,18 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Health Check')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @ApiOperation({ summary: 'Health Check' })
+  @ApiResponse({
+    status: 200,
+    description: 'Health Check',
+    schema: { type: 'string', example: 'UP!' },
+  })
   @Get()
   health_check(): string {
     return this.appService.health_check();
