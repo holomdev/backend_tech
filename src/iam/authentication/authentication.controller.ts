66ab7343b0ac47fab6d03ff467a0e168bb42329a
Fabio Sanchez <holom.dev@gmail.com>
2023-12-05 14:45:50 -0600

    feat: add sign-up and sign-in functionality to authentication controller

--- src/iam/authentication/authentication.controller.ts
+++ src/iam/authentication/authentication.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller } from '@nestjs/common';
+import { AuthenticationService } from './authentication.service';
+import { SignUpDto } from './dto/sign-up.dto';
+import { SignInDto } from './dto/sign-in.dto';
 
 @Controller('authentication')
-export class AuthenticationController {}
+export class AuthenticationController {
+  constructor(private readonly authService: AuthenticationService) {}
+
+  signUp(@Body() signUpDto: SignUpDto) {
+    return this.authService.signUp(signUpDto);
+  }
+
+  signIn(@Body() signInDto: SignInDto) {
+    return this.authService.signIn(signInDto);
+  }
+}
--- src/iam/authentication/authentication.service.ts
+++ src/iam/authentication/authentication.service.ts
@@ -1,4 +1,15 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Injectable } from '@nestjs/common';
+import { SignUpDto } from './dto/sign-up.dto';
+import { SignInDto } from './dto/sign-in.dto';
 
 @Injectable()
-export class AuthenticationService {}
+export class AuthenticationService {
+  async signUp(signUpDto: SignUpDto) {
+    return 'signUp';
+  }
+
+  async signIn(signInDto: SignInDto) {
+    return 'signIn';
+  }
+}
