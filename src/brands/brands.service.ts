7697265a9c7d5fab7a109610b719a7d95407b796
Fabio Sanchez <holom.dev@gmail.com>
2023-12-06 00:17:04 -0600

    feat: add create BrandsService

--- src/brands/brands.service.ts
+++ src/brands/brands.service.ts
@@ -1,16 +1,40 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Brand } from './entities/brand.entity';
 import { CreateBrandDto } from './dto/create-brand.dto';
 import { UpdateBrandDto } from './dto/update-brand.dto';
 
 @Injectable()
 export class BrandsService {
-  create(createBrandDto: CreateBrandDto) {
-    return 'This action adds a new brand';
+  constructor(
+    @InjectRepository(Brand)
+    private brandRepository: Repository<Brand>,
+  ) {}
+
+  async create(createBrandDto: CreateBrandDto) {
+    try {
+      const brand = this.brandRepository.create(createBrandDto);
+      return await this.brandRepository.save(brand);
+    } catch (err) {
+      if (err.code === '23505') {
+        throw new ConflictException();
+      }
+      throw err;
+    }
   }
 
-  findAll() {
-    return `This action returns all brands`;
+  async findAll() {
+    try {
+      return await this.brandRepository.find();
+    } catch (err) {
+      throw new InternalServerErrorException();
+    }
   }
 
   findOne(id: number) {
