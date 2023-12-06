2986e1db045a589fe18ce8d1a43d364beb91b34b
Fabio Sanchez <holom.dev@gmail.com>
2023-12-06 15:15:15 -0600

    feat: add authentication decorator to health check endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { Auth } from './iam/authentication/decorators/auth.decorator';
+import { AuthType } from './iam/authentication/enums/auth-type.enum';
 
 @ApiTags('Health Check')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Auth(AuthType.None)
   @ApiOperation({ summary: 'Health Check' })
   @ApiResponse({
     status: 200,
