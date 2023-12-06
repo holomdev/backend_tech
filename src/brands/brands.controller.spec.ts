135c1e1770f12d2c53902ac9b23ea8c2e1b8765f
Fabio Sanchez <holom.dev@gmail.com>
2023-12-06 14:12:59 -0600

    test: fix unit test setup in BrandsController

--- src/brands/brands.controller.spec.ts
+++ src/brands/brands.controller.spec.ts
@@ -1,17 +1,31 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Test, TestingModule } from '@nestjs/testing';
 import { BrandsController } from './brands.controller';
 import { BrandsService } from './brands.service';
 
 describe('BrandsController', () => {
   let controller: BrandsController;
+  let brandService: BrandsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [BrandsController],
-      providers: [BrandsService],
+      providers: [
+        {
+          provide: BrandsService,
+          useValue: {
+            create: jest.fn(),
+            findAll: jest.fn(),
+            findOne: jest.fn(),
+            update: jest.fn(),
+            remove: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<BrandsController>(BrandsController);
+    brandService = module.get<BrandsService>(BrandsService);
   });
 
   it('should be defined', () => {
