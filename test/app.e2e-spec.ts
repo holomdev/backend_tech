85e1300d4d90d2ca6496688439e0de431ec81ad3
Fabio Sanchez <holom.dev@gmail.com>
2023-12-05 18:36:58 -0600

    test(2e2): fix response message in root endpoint

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -27,7 +27,7 @@ describe('AppController (e2e)', () => {
   });
 
   it('/ (GET)', () => {
-    return request(app.getHttpServer()).get('/').expect(200).expect('UPP!');
+    return request(app.getHttpServer()).get('/').expect(200).expect('UP!');
   });
 
   afterAll(async () => {
