b77f02894661f2f5648cf593ae8daf567ba7559a
Fabio Sanchez <holom.dev@gmail.com>
2023-12-06 15:53:21 -0600

    feat: add ApiProperty decorator to CreateBrandDto

--- src/brands/dto/create-brand.dto.ts
+++ src/brands/dto/create-brand.dto.ts
@@ -1,6 +1,8 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, MaxLength } from 'class-validator';
 
 export class CreateBrandDto {
+  @ApiProperty({ description: 'The name of the brand' })
   @IsString()
   @MaxLength(60)
   name: string;
