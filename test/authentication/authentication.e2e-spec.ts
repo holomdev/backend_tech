f7b8a8465d9b66b6baf16307eae96e7379b7a10c
Fabio Sanchez <holom.dev@gmail.com>
2023-12-05 19:26:11 -0600

    test(2e2): add authentication e2e test for sign-up endpoint

---
+++ test/authentication/authentication.e2e-spec.ts
@@ -0,0 +1,44 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication, ValidationPipe, HttpStatus } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from '../../src/app.module';
+import { SignUpDto } from 'src/iam/authentication/dto/sign-up.dto';
+
+describe('[Feature] Authentication - /authentication (e2e)', () => {
+  let app: INestApplication;
+
+  beforeAll(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        transform: true,
+        whitelist: true,
+        transformOptions: {
+          enableImplicitConversion: true,
+        },
+      }),
+    );
+    await app.init();
+  });
+
+  it('sign-up [POST /]: should register user', async () => {
+    const user: SignUpDto = {
+      name: 'test_name',
+      email: 'test@example.com',
+      username: 'username_test',
+      password: 'password123',
+    };
+    return await request(app.getHttpServer())
+      .post('/authentication/sign-up')
+      .send(user)
+      .expect(HttpStatus.CREATED);
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+});
