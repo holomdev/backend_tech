99235540303af8386a5969e6ac682b9777507c58
Fabio Sanchez <holom.dev@gmail.com>
2023-12-06 15:54:04 -0600

    feat: add ApiProperty decorator to UpdateBrandDto

--- src/brands/dto/update-brand.dto.ts
+++ src/brands/dto/update-brand.dto.ts
@@ -1,8 +1,9 @@
-import { PartialType } from '@nestjs/swagger';
+import { ApiProperty, PartialType } from '@nestjs/swagger';
 import { CreateBrandDto } from './create-brand.dto';
 import { IsOptional, IsString, MaxLength } from 'class-validator';
 
 export class UpdateBrandDto extends PartialType(CreateBrandDto) {
+  @ApiProperty({ description: 'The new name of the brand' })
   @IsOptional()
   @IsString()
   @MaxLength(60)
