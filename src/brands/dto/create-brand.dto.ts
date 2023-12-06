6d784a727b445d64dda25dc2c0b364a10de9bee9
Fabio Sanchez <holom.dev@gmail.com>
2023-12-05 23:52:41 -0600

    feat: add name validation to CreateBrandDto and UpdateBrandDto

--- src/brands/dto/create-brand.dto.ts
+++ src/brands/dto/create-brand.dto.ts
@@ -1,1 +1,7 @@
-export class CreateBrandDto {}
+import { IsString, MaxLength } from 'class-validator';
+
+export class CreateBrandDto {
+  @IsString()
+  @MaxLength(60)
+  name: string;
+}
--- src/brands/dto/update-brand.dto.ts
+++ src/brands/dto/update-brand.dto.ts
@@ -1,4 +1,10 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateBrandDto } from './create-brand.dto';
+import { IsOptional, IsString, MaxLength } from 'class-validator';
 
-export class UpdateBrandDto extends PartialType(CreateBrandDto) {}
+export class UpdateBrandDto extends PartialType(CreateBrandDto) {
+  @IsOptional()
+  @IsString()
+  @MaxLength(60)
+  name: string;
+}
