2276ffe5d990469c4e7af0b01c3c237f032a4aa5
Fabio Sanchez <holom.dev@gmail.com>
2023-12-06 20:11:25 -0600

    feat: refactor create-product.dto.ts to use brandId instead of brand

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,14 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsString } from 'class-validator';
-import { Brand } from '../../brands/entities/brand.entity';
-import { Type } from 'class-transformer';
+import { IsNumber, IsString } from 'class-validator';
 
 export class CreateProductDto {
   @ApiProperty({ description: 'The name of the product' })
   @IsString()
   name: string;
 
-  @ApiProperty({ description: 'The brand of the product', type: () => Brand })
-  @Type(() => Brand)
-  brand: Brand;
+  @ApiProperty({ description: 'The id of the brand of the product' })
+  @IsNumber()
+  brandId: number;
 }
