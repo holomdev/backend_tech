cd4808e3399a771dedfb6be043e99c9d1065a792
Fabio Sanchez <holom.dev@gmail.com>
2023-12-06 19:49:07 -0600

    feat: add Swagger API documentation to ProductsController

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -7,34 +7,64 @@ import {
   Param,
   Delete,
 } from '@nestjs/common';
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 
+@ApiTags('Products')
+@ApiBearerAuth()
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
+  @ApiOperation({ summary: 'Create product' })
+  @ApiResponse({
+    status: 201,
+    description: 'The product has been successfully created.',
+  })
   @Post()
   create(@Body() createProductDto: CreateProductDto) {
     return this.productsService.create(createProductDto);
   }
 
+  @ApiOperation({ summary: 'Get all products' })
+  @ApiResponse({ status: 200, description: 'Return all products' })
   @Get()
   findAll() {
     return this.productsService.findAll();
   }
 
+  @ApiOperation({ summary: 'Get product by id' })
+  @ApiResponse({
+    status: 200,
+    description: 'Return the product with the corresponding id',
+  })
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.productsService.findOne(+id);
   }
 
+  @ApiOperation({ summary: 'Update product' })
+  @ApiResponse({
+    status: 200,
+    description: 'The product has been successfully updated.',
+  })
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
     return this.productsService.update(+id, updateProductDto);
   }
 
+  @ApiOperation({ summary: 'Delete product' })
+  @ApiResponse({
+    status: 200,
+    description: 'The product has been successfully deleted.',
+  })
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.productsService.remove(+id);
