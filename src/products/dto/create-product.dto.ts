faca18657426f3aa913429e4fe8d71e755705929
Fabio Sanchez <holom.dev@gmail.com>
2023-12-06 19:30:23 -0600

    feat: add name and brand properties to CreateProductDto and UpdateProductDto

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,14 @@
-export class CreateProductDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString } from 'class-validator';
+import { Brand } from '../../brands/entities/brand.entity';
+import { Type } from 'class-transformer';
+
+export class CreateProductDto {
+  @ApiProperty({ description: 'The name of the product' })
+  @IsString()
+  name: string;
+
+  @ApiProperty({ description: 'The brand of the product', type: () => Brand })
+  @Type(() => Brand)
+  brand: Brand;
+}
--- src/products/dto/update-product.dto.ts
+++ src/products/dto/update-product.dto.ts
@@ -1,4 +1,20 @@
-import { PartialType } from '@nestjs/swagger';
+import { ApiProperty, PartialType } from '@nestjs/swagger';
 import { CreateProductDto } from './create-product.dto';
+import { IsOptional, IsString } from 'class-validator';
+import { Brand } from '../../brands/entities/brand.entity';
+import { Type } from 'class-transformer';
 
-export class UpdateProductDto extends PartialType(CreateProductDto) {}
+export class UpdateProductDto extends PartialType(CreateProductDto) {
+  @ApiProperty({ description: 'The new name of the product' })
+  @IsOptional()
+  @IsString()
+  name: string;
+
+  @ApiProperty({
+    description: 'The new brand of the product',
+    type: () => Brand,
+  })
+  @Type(() => Brand)
+  @IsOptional()
+  brand: Brand;
+}
