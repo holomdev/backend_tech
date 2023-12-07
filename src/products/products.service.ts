9180a9794c743539272a5cf9035deae79fcb1614
Fabio Sanchez <holom.dev@gmail.com>
2023-12-06 20:12:50 -0600

    feat: add brand validation and create new product

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,12 +1,32 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Product } from './entities/product.entity';
+import { BrandsService } from '../brands/brands.service';
 
 @Injectable()
 export class ProductsService {
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+  constructor(
+    @InjectRepository(Product)
+    private productsRepository: Repository<Product>,
+    private readonly brandsService: BrandsService,
+  ) {}
+
+  async create(createProductDto: CreateProductDto) {
+    const brand = await this.brandsService.findOne(createProductDto.brandId);
+    if (!brand) {
+      throw new NotFoundException(
+        `Brand with id ${createProductDto.brandId} not found`,
+      );
+    }
+    const newProduct = this.productsRepository.create({
+      ...createProductDto,
+      brand,
+    });
+    return await this.productsRepository.save(newProduct);
   }
 
   findAll() {
