9ece747372a5f96cd0eaf46ae0adb5bfef62816c
Fabio Sanchez <holom.dev@gmail.com>
2023-12-06 20:12:26 -0600

    feat: add Brand entity and BrandsService to ProductsModule

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -3,10 +3,12 @@ import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Product } from './entities/product.entity';
+import { Brand } from '../brands/entities/brand.entity';
+import { BrandsService } from '../brands/brands.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Product])],
+  imports: [TypeOrmModule.forFeature([Brand, Product])],
   controllers: [ProductsController],
-  providers: [ProductsService],
+  providers: [BrandsService, ProductsService],
 })
 export class ProductsModule {}
